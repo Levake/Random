ad32e3eb8ee1a8a56b7bdf4180eb5c9e341f00e3
Levake <stars.programm@gmail.com>
2019-09-14 19:53:59 +0700

    Updates

---
+++ precache-manifest.9de119a7d47de1ae9706d24264aa5e61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Random/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "25d5cccbb1b3dbf0a8c6",
+    "url": "/Random/static/js/runtime~main.ed470760.js"
+  },
+  {
+    "revision": "d91da058d56de32fb636",
+    "url": "/Random/static/js/main.76926075.chunk.js"
+  },
+  {
+    "revision": "6c7ec0c562f701b32303",
+    "url": "/Random/static/js/2.988c6c44.chunk.js"
+  },
+  {
+    "revision": "d91da058d56de32fb636",
+    "url": "/Random/static/css/main.a5c8ecb5.chunk.css"
+  },
+  {
+    "revision": "6c7ec0c562f701b32303",
+    "url": "/Random/static/css/2.252b11a6.chunk.css"
+  },
+  {
+    "revision": "adbc692622af5d79b1c5d7b9852333d4",
+    "url": "/Random/index.html"
+  }
+];
